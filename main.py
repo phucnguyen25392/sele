e4987b1b9445469959b70867c9878cd65805c1d2
Phuc Nguyen <hellboy.1315@gmail.com>
2020-01-12 13:35:17 +0700

    Edit main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ class Test_HTMLTestRunner(unittest.TestCase):
         # suite of TestCases
         self.suite = unittest.TestSuite()
         self.suite.addTests([
-            # unittest.defaultTestLoader.loadTestsFromTestCase(campagintc.Campaign),
-            # unittest.defaultTestLoader.loadTestsFromTestCase(tagtc.Tag),
-            # unittest.defaultTestLoader.loadTestsFromTestCase(contacttc.Contact),
+            unittest.defaultTestLoader.loadTestsFromTestCase(campagintc.Campaign),
+            unittest.defaultTestLoader.loadTestsFromTestCase(tagtc.Tag),
+            unittest.defaultTestLoader.loadTestsFromTestCase(contacttc.Contact),
             unittest.defaultTestLoader.loadTestsFromTestCase(notificationtc.Notification),
             ])
 
